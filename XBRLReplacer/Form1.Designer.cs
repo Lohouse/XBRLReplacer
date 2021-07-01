9eb1f1de8ca91cee9640d06a8f04172410021703
Alexander Lohaus <alexander.lohaus@gmail.com>
2021-07-01 17:52:17 +0200

    Initial commit

---
+++ XBRLReplacer/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace XBRLReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.labelSelected = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.progressReplace = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(16, 39);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(16, 123);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(100, 20);
+            this.txtFind.TabIndex = 2;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(16, 184);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(100, 20);
+            this.txtReplace.TabIndex = 3;
+            // 
+            // labelSelected
+            // 
+            this.labelSelected.AutoSize = true;
+            this.labelSelected.Location = new System.Drawing.Point(15, 65);
+            this.labelSelected.Name = "labelSelected";
+            this.labelSelected.Size = new System.Drawing.Size(102, 13);
+            this.labelSelected.TabIndex = 5;
+            this.labelSelected.Text = "No folder selected...";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(16, 246);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 6;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(364, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "1. Select the folder that contains the XBRL files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2. Specify the ID to replace";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "3. Specify the new ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "4. Replace";
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.Location = new System.Drawing.Point(13, 315);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(0, 13);
+            this.labelAction.TabIndex = 5;
+            // 
+            // progressReplace
+            // 
+            this.progressReplace.Location = new System.Drawing.Point(16, 289);
+            this.progressReplace.Name = "progressReplace";
+            this.progressReplace.Size = new System.Drawing.Size(365, 23);
+            this.progressReplace.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 347);
+            this.Controls.Add(this.progressReplace);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.labelAction);
+            this.Controls.Add(this.labelSelected);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "XBRL ID Replacer (by Alexander Lohaus)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label labelSelected;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.ProgressBar progressReplace;
+    }
+}
+
---
+++ XBRLReplacer/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XBRLReplacer
+{
+    public partial class Form1 : Form
+    {
+        private XNamespace xbrli = "http://www.xbrl.org/2003/instance";
+        private List<string> xbrlFiles = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    foreach (var f in files)
+                    {
+                        Console.WriteLine(Path.GetExtension(f));
+                        if (Path.GetExtension(f) == ".xbrl")
+                        {
+                            xbrlFiles.Add(f);
+                        }
+                    }
+
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+                    labelSelected.Text = "Folder: " + fbd.SelectedPath + " (contains " + xbrlFiles.Count.ToString() + " XBRL files)";
+                    labelAction.Text = "";
+                    progressReplace.Value = 0;
+                }
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            if (xbrlFiles.Count <= 0)
+            {
+                System.Windows.Forms.MessageBox.Show("ERROR: No XBRL files found in the given folder!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtFind.Text) || string.IsNullOrWhiteSpace(txtReplace.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("ERROR: Find or replace field does not contain valid text!");
+                return;
+            }
+
+            if (txtFind.Text.Equals(txtReplace.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("ERROR: Find and replace are equal!");
+                return;
+            }
+
+            btnReplace.Enabled = false;
+
+            int filesReplaced = 0;
+            int amountReplaced = 0;
+            progressReplace.Maximum = xbrlFiles.Count;
+
+            foreach (var f in xbrlFiles)
+            {
+                filesReplaced++;
+
+                string[] fParts = f.Split('\\');
+                labelAction.Text = "[" + filesReplaced.ToString() + "/" + xbrlFiles.Count.ToString() + "] " + fParts[fParts.Length - 1];
+                labelAction.Refresh();
+
+                XDocument doc = XDocument.Load(f);
+
+                foreach (var element in doc.Descendants(xbrli + "identifier"))
+                {
+                    // TODO: Also do contains, startwith, endswith ?
+                    if (element.Value == txtFind.Text)
+                    {
+                        element.SetValue(txtReplace.Text);
+                        amountReplaced++;
+                    }
+                }
+
+                doc.Save(f);
+
+                progressReplace.Value = filesReplaced;
+            }
+
+            labelAction.Text = "Replaced " + amountReplaced.ToString() + " IDs in " + filesReplaced.ToString() + " XBRL files";
+            btnReplace.Enabled = true;
+        }
+    }
+}
